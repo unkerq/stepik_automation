d0c959acbfaf2cc1f612f386b2d98c548b3e24f9
unkerq <nrvrul@bk.ru>
2019-12-03 06:27:56 +0200

    first commit adding two files

---
+++ week_2/3_step6_switch_to_window.py
@@ -0,0 +1,34 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/redirect_accept.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    trollButton = browser.find_element_by_class_name('trollface.btn.btn-primary')
+    trollButton.click()
+
+    browser.switch_to.window(browser.window_handles[1])
+
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+
+    answer = browser.find_element_by_id('answer')
+    answer.send_keys(y)
+
+    button = browser.find_element_by_tag_name("button")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(10)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
---
+++ week_2/4_step8_waiting_for_text.py
@@ -0,0 +1,38 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
+import time
+import math
+
+def calc(x):
+  return str(math.log(abs(12*math.sin(int(x)))))
+
+link = "http://suninjuly.github.io/explicit_wait2.html"
+
+try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+
+    priceButton = browser.find_element_by_id('book')
+    price = WebDriverWait(browser, 12).until(
+        EC.text_to_be_present_in_element((By.ID, 'price'), '100')
+    )
+    priceButton.click()
+
+    x_element = browser.find_element_by_id('input_value')
+    x = x_element.text
+    y = calc(x)
+
+    answer = browser.find_element_by_id('answer')
+    answer.send_keys(y)
+
+    button = browser.find_element_by_id("solve")
+    button.click()
+
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(20)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
